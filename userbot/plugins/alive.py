d8abcb66ffcf08c0be9dc0a46cd3339fcd23be83
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2020-12-05 12:12:57 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,38 +1,10 @@
-import time
+
 
 import asyncio
 from telethon import events
 from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
-
-# Functions
-def get_readable_time(seconds: int) -> str:
-    count = 0
-    ping_time = ""
-    time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
-
-    while count < 4:
-        count += 1
-        if count < 3:
-            remainder, result = divmod(seconds, 60)
-        else:
-            remainder, result = divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        ping_time += time_list.pop() + ", "
-
-    time_list.reverse()
-    ping_time += ":".join(time_list)
-
-    return ping_time
 
 
 uptime = get_readable_time((time.time() - Lastupdate))
@@ -42,14 +14,12 @@ pm_caption = "⚠️ ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot is On 🔥 FIRE �⚠�
 pm_caption += "🔸**SYSTEM STATUs**\n"
 pm_caption += "🔹TELETHON VERSION : **6.0.9**\n ⭕️ Python: **3.7.4**\n"
 pm_caption += "🔸DATABASE STATUS  : **Functional**\n"
-pm_caption += f"🔸 **Uptime** : `{uptime}` \n"
 pm_caption += "➥ **Current Branch** : `master`\n"
 pm_caption += "🔸**ᔕᗴᑎᔕᗴᎥᗰᗩ᙭ OS** :   1.15`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
 pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
 pm_caption += "➥ **License** : [MIT License](https://github.com/SenseiMAX/SenseiMAX-Kingbot/blob/master/LICENSE)\n"
 pm_caption += "🔸**Made By 😎** : [This Peros](https://t.me/senseimaxxx)\n\n"
-pm_caption += "➥ **Check Stats By Doing** `.stat`. \n\n"
 pm_caption += "🔻Deploy ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot : [ℝ𝕖𝕡𝕠](https://github.com/SenseiMAX/SenseiMAX-Kingbot)\n"
 
 @borg.on(admin_cmd(pattern=r"alive"))
