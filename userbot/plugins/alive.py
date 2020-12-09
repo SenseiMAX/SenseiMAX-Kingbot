e8f89746c80c95fb6335dff0cafd3fc2a1bcdfc7
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2020-12-10 01:24:38 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -5,16 +5,16 @@ from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "SenseiMAx user"
-PM_IMG = "https://telegra.ph/file/aa9495e02bcdde118a977.gif"
-pm_caption = "`ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot IS:` **ONLINE**\n\n"
-pm_caption += "**SYSTEM STATUS**\n"
-pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.8.5**\n"
-pm_caption += "`DATABASE STATUS:` **Functional**\n"
-pm_caption += "**Current Branch** : `master`\n"
-pm_caption += "*ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot** : `3.14`\n"
-pm_caption += f"**My Boss** : {DEFAULTUSER} \n"
-pm_caption += "**Made By 😎** : [This nub}(https://t.me/senseimaxxx)\n\n"
-pm_caption += "Deploy Your Own : [R E P O](https://github.com/SenseiMAX/SenseiMAX-Kingbot)\n"
+PM_IMG = "https://telegra.ph/file/fbfa604b9b3bc436a5635.mp4"
+pm_caption = "🔺`ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot IS:` **ONLINE**\n\n"
+pm_caption += "🔻**SYSTEM STATUS**\n"
+pm_caption += "🔺`TELETHON VERSION:` **6.0.9**\n`Python:` **3.8.5**\n"
+pm_caption += "🔻`DATABASE STATUS:` **Functional**\n"
+pm_caption += "🔶**Current Branch** : `master`\n"
+pm_caption += "🔷*ᔕᗴᑎᔕᗴᎥᗰᗩ᙭-Kingbot** : `3.14`\n"
+pm_caption += f"🔹**My Boss** : {DEFAULTUSER} \n"
+pm_caption += "🔸**Made By 😎** : [This nub](https://t.me/senseimaxxx)\n\n"
+pm_caption += "👀 Deploy Your Own : [R E P O](https://github.com/SenseiMAX/SenseiMAX-Kingbot)\n"
 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def friday(alive):
